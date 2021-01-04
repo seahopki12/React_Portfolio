553979eccd7da0973fa2b68bf9b0988e677f0249
Sean Hopkins <seahopki12@gmail.com>
2021-01-04 12:35:03 -0500

    added header component and basic routing to app.js

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+// import Projects from "./pages/Projects";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+// import NoMatch from "./pages/NoMatch";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/home" component={Home} />
+            {/* <Route exact path="/projects" component={Projects} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+            <Route component={NoMatch} /> */}
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
